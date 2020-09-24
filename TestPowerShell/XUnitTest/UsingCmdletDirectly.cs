cd1f23239e8c7b8922bb32a756b879ea7ee074d1
Matthew Turner <Matthew.Turner@aveva.com>
2020-09-24 10:49:53 -0600

    invalid test and test notes

--- TestPowerShell/XUnitTest/UsingCmdletDirectly.cs
+++ TestPowerShell/XUnitTest/UsingCmdletDirectly.cs
@@ -8,6 +8,9 @@ namespace XUnitTest
 {
     public sealed class UsingCmdletDirectly
     {
+        /// <summary>
+        /// Demonstrates the ability, and how, to use a cmdlet directly.
+        /// </summary>
         [Fact]
         public void WhenHappyPathShouldOutput()
         {
@@ -17,6 +20,23 @@ namespace XUnitTest
             };
             string output = cmdlet.Invoke<string>().First<string>();
             Assert.Equal("Hello me", output);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <remarks>
+        /// As it turns out, no validation is performed on direct use.
+        /// </remarks>
+        [Fact]
+        public void WhenInvalidParameterShouldOutput()
+        {
+            var cmdlet = new TestCmdlet1.TestPowerShell1Cmdlet
+            {
+                Name = ""
+            };
+            string output = cmdlet.Invoke<string>().First<string>();
+            Assert.Equal("Hello ", output);
         }
     }
 }
