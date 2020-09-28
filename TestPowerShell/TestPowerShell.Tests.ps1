133c4e5fc1c5b1ecf6015eeac2326c75e074ec5d
Matthew Turner <Matthew.Turner@aveva.com>
2020-09-28 11:34:42 -0600

    remote use

--- TestPowerShell/TestPowerShell.Tests.ps1
+++ TestPowerShell/TestPowerShell.Tests.ps1
@@ -1,9 +1,22 @@
 Describe "Testing Binary Cmdlets" {
+    $cred = Get-Credential -Message "Pester" -UserName $ENV:USERDOMAIN\$ENV:USERNAME
     It "Is Binary Cmdlet Though PowerShell" {
-        Import-Module -Name .\bin\Debug\TestCmdlet1.dll
+        Import-Module -Name $PSScriptRoot\bin\Debug\TestCmdlet1.dll
         Test-PowerShell1 -Name me | Should BeExactly "Hello me"
     }
     It "Is Executable Using Binary Cmdlet" {
-        .\bin\Debug\TestCmdlet2.exe me | Should BeExactly "Hello me"
-    }
+        & $PSScriptRoot\bin\Debug\TestCmdlet2.exe me | Should BeExactly "Hello me"
+    
+    It "Can be Executed Remotely" {
+        $sess = New-PSSession -ComputerName $ENV:COMPUTERNAME -Authentication Credssp -Credential $cred
+        Invoke-Command -Session $sess -ScriptBlock {
+            Import-Module -Name $script:PSScriptRoot\bin\Debug\TestCmdlet1.dll
+            Test-PowerShell1 -Name me | Should BeExactly "Hello me"
+		}
+	}
+    It "Can be Imported Remotely" {
+        $sess = New-PSSession -ComputerName $ENV:COMPUTERNAME -Authentication Credssp -Credential $cred
+        Get-Item $PSScriptRoot\bin\Debug\TestCmdlet1.dll | Import-Module -PSSession $sess
+        Test-PowerShell1 -Name me | Should BeExactly "Hello me"
+	}
 }
