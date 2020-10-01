397548bcac168c8fcf9bacde242dcaeece4c62df
Matthew Turner <Matthew.Turner@aveva.com>
2020-10-01 15:13:45 -0600

    additional direct style

--- TestPowerShell/XUnitTest/UsingCmdletDirectly.cs
+++ TestPowerShell/XUnitTest/UsingCmdletDirectly.cs
@@ -38,5 +38,32 @@ namespace XUnitTest
             string output = cmdlet.Invoke<string>().FirstOrDefault<string>();
             Assert.Equal("Hello ", output);
         }
+
+        /// <summary>
+        /// Demonstrates the ability, and how, to use a cmdlet directly.
+        /// </summary>
+        [Fact]
+        public void WhenAlternateHappyPathShouldOutput()
+        {
+            var cmdlet = new TestCmdlet1.TestPowerShell1Cmdlet();
+            cmdlet.Name = "me";
+            string output = cmdlet.Invoke<string>().FirstOrDefault<string>();
+            Assert.Equal("Hello me", output);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <remarks>
+        /// As it turns out, no validation is performed on direct use.
+        /// </remarks>
+        [Fact]
+        public void WhenAlternateInvalidParameterShouldOutput()
+        {
+            var cmdlet = new TestCmdlet1.TestPowerShell1Cmdlet();
+            cmdlet.Name = "";
+            string output = cmdlet.Invoke<string>().FirstOrDefault<string>();
+            Assert.Equal("Hello ", output);
+        }
     }
 }
