f94abc177adbb19ef32129ef0f64ee17eb6df093
Matthew Turner <Matthew.Turner@aveva.com>
2021-01-08 16:26:45 -0700

    Generic Cmdlets

---
+++ GenericCmdlets/GenericCmdlets/GetIntFromCmdletCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace GenericCmdlets
+{
+    /// <summary>
+    /// <para type="notes">
+    /// However, this *can* be called by PowerShell.
+    /// </para>
+    /// </summary>
+    [Cmdlet(VerbsCommon.Get, "IntFromCmdlet")] // must be unique name
+    public sealed class GetIntFromCmdletCommand : GetFromCmdletCommand<Int32>
+    {
+    }
+}
---
+++ GenericCmdlets/TypeParameters/GetGenericListCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Reflection;
+
+namespace TypeParameters
+{
+    /// <summary>
+    /// <para type="notes">
+    /// Must use Reflection.
+    /// 
+    /// Reference: https://stackoverflow.com/questions/2604743/setting-generic-type-at-runtime/2604844#2604844
+    /// </para>
+    /// </summary>
+    [Cmdlet(VerbsCommon.Get, "GenericList")]
+    public sealed class GetGenericListCommand : Cmdlet
+    {
+        [Parameter(Mandatory = true)]
+        [Alias("Type")]
+        public Type ListType { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            Type genericListType = typeof(List<>).MakeGenericType(new Type[] { ListType }); // List<ListType>
+            ConstructorInfo genericListConstructor = genericListType.GetConstructor(new Type[] { }); // empty constructor
+            object genericList = genericListConstructor.Invoke(new object[] { }); // instantiate
+            WriteObject(genericList);
+        }
+    }
+}
