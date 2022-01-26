f50c79d9a8f84fbf63685183f0e9e06f32af60fa
carrvo <carrvo@gmail.com>
2022-01-25 18:53:17 -0700

    failed structure

---
+++ CodingStructureExample/Algorithm/GetPlusOne.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Management.Automation;
+using Example.Concrete;
+
+namespace Example.Algorithm
+{
+    [Cmdlet(VerbsCommon.Get, "PlusOne")]
+    [OutputType(typeof(Int32))]
+    public sealed class GetPlusOne : Cmdlet
+    {
+        [Parameter(Mandatory = true, ValueFromPipeline = true)]
+        public DTO InputObject { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            WriteObject(InputObject.Value + 1);
+        }
+    }
+}
---
+++ CodingStructureExample/Component/PowerShellAdapter.Tests.ps1
@@ -0,0 +1,31 @@
+﻿using assembly .\Component.dll
+using namespace Example.Component
+#Requires -Module @{ ModuleName='Pester'; RequiredVersion='4.10.1' }
+
+Describe "PowerShell Adapters" {
+	BeforeAll {
+		Import-Module $PSScriptRoot\Algorithm.psd1
+		Import-Module $PSScriptRoot\Component.psd1
+	}
+
+	It "cannot adapt a script method" {
+		$dto = Get-DTO -Value 5
+		{ $dto | Invoke-Algorithm } | Should -Throw
+	}
+
+	It "can adapt a PowerShell class" {
+		class Anonymous {
+			[int]PlusOne(){return 9}
+		}
+		New-Object Anonymous | Invoke-Algorithm | Should -Be 9
+	}
+
+	It "can accept a PowerShell class" {
+		# fails despite https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_using?view=powershell-7.2#example-3---load-classes-from-an-assembly
+		# interestingly, if pre-load module (but not `using`) it will succeed
+		class Explicit : Example.Component.IData {
+			[int]PlusOne(){return 9}
+		}
+		New-Object Explicit | Invoke-Algorithm | Should -Be 9
+	}
+}
